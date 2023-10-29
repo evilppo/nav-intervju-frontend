227c8c7ee01cfff527d7c775afb22dc0c8c9ef92
Eirik Vilpponen <eirik.vilpponen@clave.no>
2023-10-29 07:34:20 +0100

    attempt at client side component. CORS issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,6 @@
-import {GetServerSideProps} from "next";
-export async function getServerSideProps() {
-    // Fetch data from an API, a database, or any other source.
-    // For example, fetching a list of items:
-    const token: string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ"
-
-    const requestHeaders = new Headers({
-        'Authorization': `Bearer ${token}`
-    });
-
-    const response = await fetch('https://arbeidsplassen.nav.no/public-feed/api/v1/ads', {headers: requestHeaders});
-    const data = await response.json();
-
-    // Return the data as props to your component.
-    return {
-        props: {
-            items: data,
-        },
-    };
-}
-
-
+import React from "react";
+import clientComponent from "./ClientComponent";
+import ClientComponent from "./ClientComponent";
 export default function Home(props: any) {
 
 
@@ -41,8 +22,8 @@ export default function Home(props: any) {
     }
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1>HELLO WORLD</h1>
-            {renderItems()}
+
+            <ClientComponent/>
         </main>
     )
 }
