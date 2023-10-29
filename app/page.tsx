e1c63674d38d11fb213e6fd320434ea6950a78bc
Eirik Vilpponen <eirik.vilpponen@clave.no>
2023-10-29 20:40:20 +0100

    Server side fetching working

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,23 @@
 import React from "react";
-import clientComponent from "./ClientComponent";
-import ClientComponent from "./ClientComponent";
-export default function Home(props: any) {
 
+async function getData() {
+    const token: string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ"
+    const requestHeaders = new Headers({
+        'Authorization': `Bearer ${token}`
+    });
+    const response = await fetch('https://arbeidsplassen.nav.no/public-feed/api/v1/ads', {headers: requestHeaders});
+    return response.json();
+}
+export default async function Page() {
 
-    console.log(props.items)
+    const data = await getData()
+    console.log("data.totalElements: " + data?.totalElements)
 
-    const renderItems = () => {
-        if (props.items) {
-            return props.items.map((item: any) => {
-                return (
-                    <div key={item.id}>
-                        <h1>{item.data}</h1>
-                        <p>{item.text}</p>
-                    </div>
-                )
-            })
-        } else {
-            return <div>no items</div>
-        }
-    }
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-            <ClientComponent/>
+            <p>Forsiden</p>
+            <p>data totalelements: {data? data.totalElements :"no data"}</p>
         </main>
     )
 }
