75892fc1d4e74cacd79d009024d66cd4904d7a5a
Eirik Vilpponen <eirik.vilpponen@clave.no>
2023-10-30 10:19:13 +0100

    Prettier linting

--- app/page.tsx
+++ app/page.tsx
@@ -1,87 +1,87 @@
-import React from "react";
+import React from 'react';
 
 async function getData() {
-    const token: string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ"
-    const requestHeaders = new Headers({
-        'Authorization': `Bearer ${token}`
-    });
-    const response = await fetch('https://arbeidsplassen.nav.no/public-feed/api/v1/ads?size=100', {headers: requestHeaders});
-    return response.json();
+  const token: string =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwdWJsaWMudG9rZW4udjFAbmF2Lm5vIiwiYXVkIjoiZmVlZC1hcGktdjEiLCJpc3MiOiJuYXYubm8iLCJpYXQiOjE1NTc0NzM0MjJ9.jNGlLUF9HxoHo5JrQNMkweLj_91bgk97ZebLdfx3_UQ';
+  const requestHeaders = new Headers({
+    Authorization: `Bearer ${token}`,
+  });
+  const response = await fetch(
+    'https://arbeidsplassen.nav.no/public-feed/api/v1/ads?size=100',
+    { headers: requestHeaders }
+  );
+  return response.json();
 }
 
-
 interface OccupationCategory {
-    level1: string;
-    level2: string;
+  level1: string;
+  level2: string;
 }
 
 interface OccupationCategories {
-    occupationCategories: OccupationCategory[];
+  occupationCategories: OccupationCategory[];
 }
 
 interface Add {
-    uuid: string,
-    published: string,
-    expires: string,
-    updated: string,
-    workLocations: string[],
-    "title": string
-    "description": string,
-    "sourceurl": string,
-    "source": string,
-    "applicationUrl": string,
-    "applicationDue": string,
-    "occupationCategories": OccupationCategories,
-    "jobtitle": string,
-    "link": string,
-    "employer": {},
-    "engagementtype": string,
-    "extent": string,
-    "starttime": string,
-    "positioncount": string,
-    "sector": string
+  uuid: string;
+  published: string;
+  expires: string;
+  updated: string;
+  workLocations: string[];
+  title: string;
+  description: string;
+  sourceurl: string;
+  source: string;
+  applicationUrl: string;
+  applicationDue: string;
+  occupationCategories: OccupationCategories;
+  jobtitle: string;
+  link: string;
+  employer: {};
+  engagementtype: string;
+  extent: string;
+  starttime: string;
+  positioncount: string;
+  sector: string;
 }
 
 const displaySingleAd = (add: Add) => {
-    return (
-        <div>
-            <p>{add.title}</p>
-            <p>{add.description}</p>
-            <p>{add.sourceurl}</p>
-            <p>{add.source}</p>
-            <p>{add.applicationUrl}</p>
-            <p>{add.applicationDue}</p>
-            <p>{add.jobtitle}</p>
-            <p>{add.link}</p>
-            <p>{add.engagementtype}</p>
-            <p>{add.extent}</p>
-            <p>{add.starttime}</p>
-            <p>{add.positioncount}</p>
-            <p>{add.sector}</p>
-        </div>
-    )
-}
-
+  return (
+    <div>
+      <p>{add.title}</p>
+      <p>{add.description}</p>
+      <p>{add.sourceurl}</p>
+      <p>{add.source}</p>
+      <p>{add.applicationUrl}</p>
+      <p>{add.applicationDue}</p>
+      <p>{add.jobtitle}</p>
+      <p>{add.link}</p>
+      <p>{add.engagementtype}</p>
+      <p>{add.extent}</p>
+      <p>{add.starttime}</p>
+      <p>{add.positioncount}</p>
+      <p>{add.sector}</p>
+    </div>
+  );
+};
 
 const displayAds = (ads: Add[]) => {
-    return ads.map(add => displaySingleAd(add))
-}
+  return ads.map((add) => displaySingleAd(add));
+};
 export default async function Page() {
+  const data = await getData();
+  console.log('data.totalElements: ' + data?.totalElements);
 
-    const data = await getData()
-    console.log("data.totalElements: " + data?.totalElements)
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <p>Forsiden</p>
+      <p>data totalelements: {data ? data.totalElements : 'no data'}</p>
+      <p>data pageNumber: {data ? data.pageNumber : 'no data'}</p>
+      <p>data pageSize: {data ? data.pageSize : 'no data'}</p>
+      <p>data first: {data ? data.first : 'no data'}</p>
+      <p>data last: {data ? data.last : 'no data'}</p>
 
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-            <p>Forsiden</p>
-            <p>data totalelements: {data ? data.totalElements : "no data"}</p>
-            <p>data pageNumber: {data ? data.pageNumber : "no data"}</p>
-            <p>data pageSize: {data ? data.pageSize : "no data"}</p>
-            <p>data first: {data ? data.first : "no data"}</p>
-            <p>data last: {data ? data.last : "no data"}</p>
-
-            {data ? displayAds(data.content) : "no data"}
-        </main>
-    )
+      {data ? displayAds(data.content) : 'no data'}
+    </main>
+  );
 }
